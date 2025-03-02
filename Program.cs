7189412c961dbca299e6b14d25e9778cf0b2ef73
Akhilesh Bairwa <a9977134203@gmail.com>
2025-03-02 22:33:41 +0530

    write student class

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApp1
 {
     class Program
-    {
+    {   
+        class Student
+        {
+            String name;
+            int rollnumber;
+            String course;
+            int age;
+
+            public void printDetails(String name, int rollnumber,String course ,int age)
+            {
+                Console.WriteLine("Student name: " + name);
+                Console.WriteLine("Student rollnumber: " + rollnumber);
+                Console.WriteLine("Student course: " + course);
+                Console.WriteLine("Student age: " + age);
+            }
+        }
         static void Main(string[] args)
         {
+            Student s1 = new Student();
+            Student s2 = new Student();
+            Student s3 = new Student();
+
+            s1.printDetails("john",12,"java",20);
+            s2.printDetails("alice", 12, "java", 20);
+            s3.printDetails("Bob", 12, "java", 20);
+
         }
     }
 }

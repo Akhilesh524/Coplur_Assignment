aafb9fca3d561394036e26e488e6acc6dbc6450d
Akhilesh Bairwa <a9977134203@gmail.com>
2025-04-20 20:45:35 +0530

    react-app

---
+++ my-app/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+function Footer() {
+  return (
+    <div className='footer'>
+      Footer section
+    </div>
+  )
+}
+
+export default Footer
---
+++ my-app/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './App.css'
+function Header() {
+  return (
+    <div className='header'>Hello Techies</div>
+  )
+}
+
+export default Header;
